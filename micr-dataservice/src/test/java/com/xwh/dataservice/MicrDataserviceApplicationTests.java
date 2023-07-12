31afd70e50297b289eb05b5e840e85e2190733c4
血无痕 <cfy555388com@163.com>
2023-07-12 20:24:52 +0800

    优化配置

--- micr-dataservice/src/test/java/com/xwh/dataservice/MicrDataserviceApplicationTests.java
+++ micr-dataservice/src/test/java/com/xwh/dataservice/MicrDataserviceApplicationTests.java
@@ -3,13 +3,14 @@ package com.xwh.dataservice;
 import com.xwh.api.pojo.BaseInfo;
 import com.xwh.api.service.PlatBaseInfoService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class MicrDataserviceApplicationTests {
 
-    @Autowired
+    @Resource
     private PlatBaseInfoService platBaseInfoService;
 
     @Test
