161640dc3107d3c442382afff77a562ecdb1210f
YHWINS\11011395 <1984451361@qq.com>
2023-07-18 14:24:31 +0800

    修改配置

---
+++ micr-common/src/main/java/com/xwh/common/config/YamlPropertySourceFactory.java
@@ -0,0 +1,40 @@
+package com.xwh.common.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @Description: 重写读取yml配置(springboot中PropertySource的默认实现是properties类型文件的解析)
+ * @Prject: booway_software
+ * @Package: com.booway.config
+ * @author: wanjun
+ */
+public class YamlPropertySourceFactory extends DefaultPropertySourceFactory
+{
+
+    public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
+        String sourceName = name != null ? name : resource.getResource().getFilename();
+        if (!resource.getResource().exists()) {
+            return new PropertiesPropertySource(sourceName, new Properties());
+        } else if (sourceName.endsWith(".yml") || sourceName.endsWith(".yaml")) {
+            Properties propertiesFromYaml = loadYml(resource);
+            return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+        } else {
+            return super.createPropertySource(name, resource);
+        }
+    }
+
+    private Properties loadYml(EncodedResource resource) throws IOException {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(resource.getResource());
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+}
+
